e83d6c6975980fd0de3d9d8162f3c07c8698b0ec
Dani <octaramadhanirinantoro@gmail.com>
2024-05-28 21:16:35 +0700

    add pagination in list meme

--- src/app/api/list-meme/route.ts
+++ src/app/api/list-meme/route.ts
@@ -3,15 +3,20 @@ import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest, res: NextResponse) {
 
+    const { searchParams } = req.nextUrl;
+    const page = searchParams.get("page") ?? 1;
+
     try {
         const result = await pool.query(`
             SELECT users.username, users.image, memes.imagememe, memes.caption
             FROM memes
             JOIN users ON memes.userid = users.id
             ORDER BY memes.id DESC
+            OFFSET (${page} - 1) * 8 LIMIT 8
         `);
-        // console.log(result);
-        return NextResponse.json({status: 200, success: true, data: result.rows} , {
+
+        const count = await pool.query(`SELECT COUNT(*) FROM memes`);
+        return NextResponse.json({status: 200, success: true, data: result.rows, totalPages: count.rows[0].count} , {
             headers: {
               'Cache-Control': 'no-store, max-age=0',
             }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,35 +4,34 @@ import Image from "next/image";
 import { Navbar } from "@/components/navbar";
 import { useEffect, useState } from "react";
 import { RealMemeCard } from "@/components/realmemecard";
+import { Pagination } from "@/components/pagination";
 
 export default function Home() {
 
   const [memes, setMemes] = useState<any[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     const fetchMemes = async () => {
-      const response = await fetch(`/api/list-meme`,{
-        method: "GET",
-        cache: "no-store",
-        headers: {
-          'Cache-Control': 'no-store'
-        }
-      });
+      const response = await fetch(`/api/list-meme?page=${currentPage}`);
       const data = await response.json();
       setMemes(data.data);
+      setTotalPages(data.totalPages / 8 + 1);
     };
     fetchMemes();
-  }, []);
+  }, [currentPage]);
 
   return (
     <>
     <Navbar />
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="grid grid-cols-1 gap-4 w-full max-w-screen-lg">
-          {memes.map((meme, index) => (
+          {memes?.map((meme, index) => (
             <RealMemeCard key={index} meme={meme} />
           ))}
         </div>
+        <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
     </main>
     </>
   );
